3b3e0274a6882c8366276420aa7669464b365171
jeff283 <njorogejeff2019@gmail.com>
2025-08-24 23:59:06 +0300

    fix: Ensure DATABASE_URL is defined before initializing the database connection; add environment variables to turbo.json outputs

--- apps/web-app/src/db/drizzle/index.ts
+++ apps/web-app/src/db/drizzle/index.ts
@@ -2,10 +2,10 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { schema } from "./schema";
 
-const dbUrl = process.env.DATABASE_URL!;
-// if (!dbUrl) {
-//   throw new Error("DATABASE_URL is not defined");
-// }
+const dbUrl = process.env.DATABASE_URL;
+if (!dbUrl) {
+  throw new Error("DATABASE_URL is not defined");
+}
 
 const client = postgres(dbUrl, { prepare: false });
 export const db = drizzle(client, {
